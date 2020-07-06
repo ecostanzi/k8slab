51d3031d72880631ec1752ede60bcafea812805d
Enrico Costanzi <enrico.costanzi@gmail.com>
2020-07-06 21:38:20 +0200

    env variable for product service

--- product/src/main/java/org/ecostanzi/product/ProductController.java
+++ product/src/main/java/org/ecostanzi/product/ProductController.java
@@ -2,6 +2,7 @@ package org.ecostanzi.product;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,11 +15,17 @@ public class ProductController {
 
     private final Logger logger = LoggerFactory.getLogger(ProductController.class);
 
+    private final Environment environment;
+
+    public ProductController(Environment environment) {
+        this.environment = environment;
+    }
+
     @GetMapping("/products")
     public List<Product> getProducts() throws UnknownHostException {
         var ip = InetAddress.getLocalHost();
         var hostname = ip.getHostName();
-        logger.info("Getting the list of products  from {}", hostname);
+        logger.info("Getting the list of products  from shelve {}, host {}", environment.getProperty("shelf", "UNKNOWN"), hostname);
         return List.of(
                 new Product("Book1", 12.2d),
                 new Product("Book2", 12.2d)
