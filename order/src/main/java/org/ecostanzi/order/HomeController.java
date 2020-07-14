5893a88dac391645222a93bf99e7629a8568f04f
Enrico Costanzi <enrico.costanzi@gmail.com>
2020-07-14 17:26:29 +0200

    other stuff

--- order/src/main/java/org/ecostanzi/order/HomeController.java
+++ order/src/main/java/org/ecostanzi/order/HomeController.java
@@ -3,6 +3,7 @@ package org.ecostanzi.order;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,6 +12,8 @@ import java.util.List;
 
 @RestController
 public class HomeController {
+
+    final String hostname = System.getenv().getOrDefault("HOSTNAME", "unknown");
 
     private final Logger logger = LoggerFactory.getLogger(HomeController.class);
     private final RestTemplate restTemplate;
@@ -24,5 +27,31 @@ public class HomeController {
         logger.info("Load order page");
         Product[] products = restTemplate.getForObject("/products", Product[].class);
         return Arrays.asList(products);
+    }
+
+    @GetMapping("/sysresources")
+    public String getSystemResources() {
+        long memory = Runtime.getRuntime().maxMemory();
+        int cores = Runtime.getRuntime().availableProcessors();
+        System.out.println("/sysresources " + hostname);
+        return
+                " Memory: " + (memory / 1024 / 1024) +
+                        " Cores: " + cores + "\n";
+    }
+
+    @GetMapping("/dostuff")
+    public String doStuff() {
+        long start = System.currentTimeMillis();
+        mySlowFunction(20);
+        long ms = System.currentTimeMillis() - start;
+        return "doing stuff took " + ms + "ms";
+    }
+
+    void mySlowFunction(int baseNumber) {
+        System.out.println("mySlowFunction");
+        int result = 0;
+        for (var i = Math.pow(baseNumber, 7); i >= 0; i--) {
+            result += Math.atan(i) * Math.tan(i);
+        };
     }
 }
